6ca00f7728a333de3878d8fa75a4890e14947ebc
Naeem420-creator <buttn3849@gmail.com>
2025-09-02 11:38:39 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 1243
+let accountEmail = "neemi@gmail.com"
+var accountPassword = "1243"
+accountCity = "Sialkot"
+
+
+accountEmail = "zak@gmail.com"
+accountPassword = "12321"
+accountCity = "Sambrial"
+
+//console.log(accountId);
+
+console.table( [accountEmail, accountId, accountPassword, accountCity] )
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //trat all js code as new version
+
+// alert ( 3 + 3 )// we are using nodejs, not browser
+
+
+let name = "Naeem"
+let age = 22
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => " "/' '
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object
+
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
