4e62b70dab5691dfb568931d5d75dacbae466a12
Naeem420-creator <buttn3849@gmail.com>
2025-09-03 06:00:44 +0000

    datatypes-summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -12,7 +12,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+//console.log(id === anotherId);
 
 // const bigNumber = 3456543576654356754n
 
@@ -29,9 +29,35 @@ let myObj = {
 }
 
 const myFunction = function(){
-    console.log("Hello world");
+    //console.log("Hello world");
 }
 
-console.log(typeof anotherId);
+//console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack(Primitive),  Heap(Non-Primitive)
+
+
+let myName = "Naeem"
+// console.log(myName)
+
+let anotherName = "myName"
+anotherName = "subhan"
+
+// console.log(myName)
+// console.log(anotherName)
+
+let userOne = {
+    email: "user@gmail.com",
+    upid: "ydl@dj"
+}
+
+let userTwo = userOne
+
+userTwo.email = "naeem@gmail.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
