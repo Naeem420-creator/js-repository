5e87791b170bcbadb363254d75cdc6ad74855bd0
Naeem420-creator <buttn3849@gmail.com>
2025-09-03 05:19:27 +0000

    comparison

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,6 +1,24 @@
 let score = "43"
 
-console.log(typeof score);
+//console.log(typeof score);
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
+//console.log(typeof valueInNumber);
+
+
+// ********************* Operations *********************
+
+let value = 3
+let negValue = -value
+
+//console.log(-value)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+
+
