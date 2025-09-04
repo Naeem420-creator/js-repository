2a55cbea7b04f4a2738642e49b4911ad7423bee4
Naeem420-creator <buttn3849@gmail.com>
2025-09-04 09:39:58 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,23 @@
+const name = "Naeem"
+const age = 22
+
+// console.log(name + age)
+
+// console.log(`Hello my name is ${name} and my age is ${age}`)
+
+const gameName = new String('NaeemButt')
+
+// console.log(gameName[0])
+
+// console.log(gameName.length)
+// console.log(gameName.charAt(4))
+
+const newString = gameName.substring(0, 4)
+const anotherString = gameName.slice(-8, 4)
+
+// console.log(newString)
+// console.log(anotherString)
+
+const newStringOne = "    Naeem      "
+console.log(newStringOne)
+console.log(newStringOne.trim())
