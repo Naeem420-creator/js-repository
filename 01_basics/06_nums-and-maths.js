e4966708390eb0965aa80c14a1ce1170583d290c
Naeem420-creator <buttn3849@gmail.com>
2025-09-04 10:17:29 +0000

    Math and Num

---
+++ 01_basics/06_nums-and-maths.js
@@ -0,0 +1,29 @@
+const score = 389
+const balance = new Number(324)
+
+// console.log(score)
+// console.log(balance)
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+// const otherNumber = 427.9482
+// console.log(otherNumber.toPrecision(5))
+
+// +++++++++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-6))
+// console.log(Math.round(5.8))
+// console.log(Math.ceil(5.1))
+// console.log(Math.floor(5.9))
+// console.log(Math.min(3,4,1,53,9))
+// console.log(Math.max(3,4,1,53,5))
+
+// console.log(Math.random())
+// console.log((Math.random()*10) + 1)
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor (Math.random() * (max - min + 1)) + min)
